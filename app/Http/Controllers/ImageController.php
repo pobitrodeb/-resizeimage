fe979b340309238da126949c797b86fcfb53eb52
Pobitrodeb <pobitrodn@gmail.com>
2023-03-14 23:37:16 +0600

    multile file added

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -36,4 +36,34 @@ class ImageController extends Controller
             ->with('success','Image Upload successful')
             ->with('imageName',$imageName);
     }
+
+    // Multiple data add
+    public function store(Request $request)
+    {
+
+
+        $this->validate($request, [
+                'filenames' => 'required',
+                'filenames.*' => 'mimes:doc,pdf,docx,zip'
+        ]);
+
+
+        if($request->hasfile('filenames'))
+         {
+            foreach($request->file('filenames') as $file)
+            {
+                $name = time().'.'.$file->extension();
+                $file->move(public_path().'/files/', $name);
+                $data[] = $name;
+            }
+         }
+
+
+         $file= new File();
+         $file->filenames=json_encode($data);
+         $file->save();
+
+
+        return back()->with('success', 'Data Your files has been successfully added');
+    }
 }
