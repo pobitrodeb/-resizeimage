1538e672f174091f0edc6d159844c8a40736b5ec
Pobitrodeb <pobitrodn@gmail.com>
2023-03-14 22:59:07 +0600

    controller added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ImageController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::controller(ImageController::class)->group(function(){
+    Route::get('image-upload', 'index');
+    Route::post('image-upload', 'store')->name('image.store');
 });
