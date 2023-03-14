3746e69e2df17c5f62b6fdfa500b569f6b9a1ad4
Pobitrodeb <pobitrodn@gmail.com>
2023-03-14 23:22:58 +0600

    image stored

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Image;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class ImageController extends Controller
 {
@@ -11,8 +14,26 @@ class ImageController extends Controller
         return view('imageUpload');
     }
 
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        
+        $this->validate($request, [
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        $image = $request->file('image');
+        $imageName = time().'.'.$image->extension();
+
+        $destinationPathThumbnail = public_path('/thumbnail');
+        $img = Image::make($image->path());
+        $img->resize(100, 100, function ($constraint) {
+            $constraint->aspectRatio();
+        })->save($destinationPathThumbnail.'/'.$imageName);
+
+        $destinationPath = public_path('/images');
+        $image->move($destinationPath, $imageName);
+
+        return back()
+            ->with('success','Image Upload successful')
+            ->with('imageName',$imageName);
     }
 }
